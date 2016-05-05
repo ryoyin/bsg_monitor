1e9337f862fa2a488ac7906e60386dc3f0171ea6
unknown <Administrator@PC08.BeijingSecGroup.local>
2016-05-05 14:07:34 +0800

    remove schedule test ping

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,8 +35,7 @@ class Kernel extends ConsoleKernel
                 "domain_server" => array("fullname" => "域和文件服务器", "ip" => "192.168.10.11"),
                 "mail_server"   => array("fullname" => "邮件服务器", "ip" => "192.168.10.12"),
                 "ricoh_printer" => array("fullname" => "Ricoh 打印机", "ip" => "192.168.10.254"),
-                "cctv"          => array("fullname" => "CCTV 閉路電視", "ip" => "192.168.10.31"),
-                "test"          => array("fullname" => "Test 測試", "ip" => "192.168.10.253")
+                "cctv"          => array("fullname" => "CCTV 閉路電視", "ip" => "192.168.10.31")
             );
 
             foreach($server_list AS $server) {
