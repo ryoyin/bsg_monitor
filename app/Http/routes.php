67c27ae7669fde1b17b20b0e2e832ee1024d508d
unknown <Administrator@PC08.BeijingSecGroup.local>
2016-05-05 14:02:49 +0800

    change mail setting and test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -36,3 +36,34 @@ Route::group(['middleware' => ['web']], function () {
 
 });
 
+Route::get('servertest', function() {
+
+    $server_list = array(
+        "vm_server"     => array("fullname" => "虚拟伺服器", "ip" => "192.168.10.10"),
+        "domain_server" => array("fullname" => "域和文件服务器", "ip" => "192.168.10.11"),
+        "mail_server"   => array("fullname" => "邮件服务器", "ip" => "192.168.10.12"),
+        "ricoh_printer" => array("fullname" => "Ricoh 打印机", "ip" => "192.168.10.254"),
+        "cctv"          => array("fullname" => "CCTV 閉路電視", "ip" => "192.168.10.31"),
+        "test"          => array("fullname" => "Test 測試", "ip" => "192.168.10.253")
+    );
+
+    foreach($server_list AS $server) {
+
+        $command = "ping ".$server['ip']." -c 1";
+
+        exec($command, $output, $return_var);
+
+        if(strstr($output[4], '1 received') === FALSE) {
+
+            Mail::raw('Ping fail to server: '.$server['fullname'], function ($message) use ($server) {
+                $message->from('royho@beijingsecgroup.com');
+                $message->to('royho@beijingsecgroup.com')->subject($server['fullname'].'Ping Fail!');
+            });
+
+        }
+
+        unset($output);
+
+    }
+
+});
