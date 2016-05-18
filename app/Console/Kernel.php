0b1fa349fd3f0c1c7f66e9f2440eb76b721e017e
Roy Ho <kwanyin2000@gmail.com>
2016-05-18 10:42:03 +0800

    Remove Test Connection

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -38,8 +38,8 @@ class Kernel extends ConsoleKernel
                 "cctv"             => array("fullname" => "CCTV 閉路電視", "ip" => "192.168.10.31"),
                 "avaya_ms_server"  => array("fullname" => "Avaya MS Window Server 2008", "ip" => "192.168.10.20"),
                 "avaya_app_server" => array("fullname" => "Avaya Application Server", "ip" => "192.168.10.19"),
-                "avaya_ipo"        => array("fullname" => "Avaya IPO Server", "ip" => "192.168.10.18"),
-                "test"             => array("fullname" => "Testing Connection", "ip" => "192.168.10.15")
+                "avaya_ipo"        => array("fullname" => "Avaya IPO Server", "ip" => "192.168.10.18")
+                //"test"             => array("fullname" => "Testing Connection", "ip" => "192.168.10.15")
             );
 
             foreach($server_list AS $server) {
